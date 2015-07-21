4571a9842e18412ef5a43a1dbcbf986e1b6a3492
Zhiyang <zhiyanghe96@gmail.com>
2015-07-20 22:43:16 -0700

    Better Sabrina Animation

--- public/js/index.js
+++ public/js/index.js
@@ -19,11 +19,10 @@ $(window).scroll(function() {
 
 	$('.case-slideRight').each(function(){
 		var imagePos = $(this).offset().top;
+		console.log(topOfWindow - imagePos);
 
-		if (imagePos < topOfWindow+400) {
+		if (imagePos < topOfWindow+600) {
 			$(this).addClass("slideRight");
 		}
 	});
-
-    $( ".dialog" ).dialog();
 });
